aa2f4d3ab36492d57f86e31334f77949b4ec5b15
Leonardo Bitzki <ljbitzki@gmail.com>
2025-05-25 14:45:04 -0300

    Update experimento-sf-install.sh

--- experimento-sf-install.sh
+++ experimento-sf-install.sh
@@ -65,6 +65,11 @@ else
   echo -e "Dependência \e[32mdocker\e[0m já está na versão mínima necessária..."
 fi
 
+echo -e '\e[33mParando e removendo quaisquer contêiners antigos\e[0m'
+while read CID; do
+	docker rm -f "${CID}"
+done < <( docker ps -a | grep -v 'CONTAINER ID' | awk '{print $1}' )
+
 mkdir -p "/tmp/experimento-sf"
 cd "/tmp/experimento-sf" || exit 1
 echo -e '\e[33mClonando repositório do Netbox\e[0m'
@@ -77,13 +82,13 @@ services:
       - 8080:8080
 EOF
 echo -e '\e[33mBaixando imagens do Netbox\e[0m'
-docker compose pull
 echo -e '\e[33mIniciando componentes do Netbox\e[0m'
 docker compose up -d
 echo -e '\e[33mParando banco de dados do Netbox e copiando modelo pronto\e[0m'
 docker stop netbox-docker-postgres-1
 wget "https://github.com/ljbitzki/ljbitzki.github.io/raw/refs/heads/master/netbox-postgresql.tar.gz" -O netbox-postgresql.tar.gz
 tar xzf netbox-postgresql.tar.gz
+echo -e 'Caso seja solicitado senha para esta etapa, a senha é \e[95mexperimento\e[0m\n'
 sudo rsync -Crazp var/lib/docker/volumes/netbox-docker_netbox-postgres-data/ /var/lib/docker/volumes/netbox-docker_netbox-postgres-data/
 echo -e '\e[33mParando Netbox\e[0m'
 docker compose down
@@ -106,13 +111,15 @@ docker run -d --network netbox-docker_default --name=ubuntu-rogue ljbitzki/sbseg
 echo -e '\e[36m******************\e[0m'
 echo -e '\e[36mAmbiente iniciado!\e[0m'
 echo -e '\e[36m******************\e[0m\n'
-sleep 1
+sleep 2
 echo -e '\nPara abrir o SSoT (Netbox), abra no navegador: \n\e[33mhttp://localhost:8080/ipam/services/\e[0m\nUsuário: \e[32madmin\e[0m\nSenha: \e[32madmin\e[0m\n'
-sleep 1
+sleep 1.5
 echo -e 'Para abrir o Grafana e acompanhar o gráfico de requisições por segundo, abra no navegador: \n\e[33mhttp://localhost:3000/public-dashboards/7d7b1678f7e94829a1816723c251e934?refresh=auto\e[0m\n'
-sleep 1
-echo -e 'Quando quiser simular um \e[95mataque\e[0m, execute em uma nova aba no terminal: \n\e[33mdocker exec -it ubuntu-rogue /usr/local/bin/dos.sh\e[0m\n'
-sleep 1
-echo -e 'Observe no gráfico do Grafana o volume de acessos subindo e sendo interrompido. Observe também a criação de um serviço de \e[31mDROP\e[0m no Netbox, correspondente ao bloqueio do atacante.\e[0m\n'
-sleep 1
-echo -e 'Para reiniciar o experimento, delete o serviço \e[32mDoS\e[0m no Netbox e execute novamente \e[33mdocker exec -it ubuntu-rogue /usr/local/bin/dos.sh\e[0m para um novo ataque.\n'
+sleep 1.5
+echo -e 'Quando quiser simular um \e[95mataque\e[0m, execute em uma nova aba no terminal: \n\e[33miniciar-ataque\e[0m e pressione Enter.\n'
+sleep 1.5
+echo -e 'No navegador, observe no gráfico do Grafana o volume de acessos subindo e sendo interrompido pela implementação da regra reativa no firewall. Observe também a criação de um serviço de \e[31mDROP\e[0m no Netbox, correspondente ao bloqueio do atacante.\e[0m\n'
+sleep 1.5
+echo -e 'Para reiniciar o experimento, pressione Ctrl+C no terminal do comando \e[33miniciar-ataque\e[0m e delete o serviço \e[32mDoS\e[0m no Netbox.'
+sleep 1.5
+echo -e 'Execute novamente \e[33miniciar-ataque\e[0m para uma nova simulação.\n'
